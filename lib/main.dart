cccd5a43393379e29a6091411d27df80bdeba416
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-01-11 17:13:11 -0300

    agregando navegacion

--- lib/main.dart
+++ lib/main.dart
@@ -52,8 +52,53 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentPageIdx = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    List pages = [GeneratorPage(), Placeholder()];
+
+    Widget currentPage = pages[_currentPageIdx];
+
+    return Scaffold(
+        body: Row(
+      children: [
+        SafeArea(
+            child: NavigationRail(
+          extended: false,
+          destinations: [
+            NavigationRailDestination(
+                icon: Icon(Icons.home), label: Text("Home")),
+            NavigationRailDestination(
+                icon: Icon(Icons.favorite), label: Text("Favourites")),
+          ],
+          selectedIndex: _currentPageIdx,
+          onDestinationSelected: (selectedId) {
+            setState(() {
+              _currentPageIdx = selectedId;
+            });
+          },
+        )),
+        Expanded(
+            child: Container(
+          color: Theme.of(context).colorScheme.primaryContainer,
+          child: currentPage,
+        ))
+      ],
+    ));
+  }
+}
+
+class GeneratorPage extends StatelessWidget {
+  const GeneratorPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -63,8 +108,7 @@ class HomePage extends StatelessWidget {
 
     bool isLiked = appState.isLiked();
 
-    return Scaffold(
-        body: Center(
+    return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
@@ -75,7 +119,7 @@ class HomePage extends StatelessWidget {
           _interactiveButtons(appState, isLiked)
         ],
       ),
-    ));
+    );
   }
 
   Row _interactiveButtons(MyAppState appState, bool isLiked) {
